0917f8912252daba955ee88692ef80bf9db4671f
GabrieleGrossi <grossigabriele98@gmail.com>
2023-10-05 17:10:41 +0200

    create new project - add Main only for try

---
+++ src/components/AppMain.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>
+            Sono il main 
+        </h1>        
+    </div>
+</template>
+<script>
+export default {
+    name: 'AppMain'
+}
+</script>
+<style lang="scss">
+    
+</style>
