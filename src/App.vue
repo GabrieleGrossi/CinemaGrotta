abd6a88eb2b3e94861426fe3f15441d5672f99bd
GabrieleGrossi <grossigabriele98@gmail.com>
2023-10-05 17:21:38 +0200

    Created header, main and footer

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,30 @@
 <script>
 import AppMain from './components/AppMain.vue';
+import AppHeader from './components/AppHeader.vue';
+import AppFooter from './components/AppFooter.vue';
 export default{
   name: 'App',
   components:{
-    AppMain
+    AppMain,
+    AppHeader,
+    AppFooter,
   }
 }
 </script>
 
 <template>
-  <AppMain/>
+  <header>
+    <AppHeader/>
+  </header>
+  <main>
+    <AppMain/>
+  </main>
+  <footer>
+    <AppFooter/>
+  </footer>
 </template>
 
 <style lang="scss">
-@use './styles/general.scss';
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+@use './styles/general.scss' as *;
+
 </style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>
+            Sono l'header 
+        </h1>
+    </div>
+</template>
+<script>
+export default {
+    name: 'AppHeader'
+}
+</script>
+<style lang="scss">
+    
+</style>
